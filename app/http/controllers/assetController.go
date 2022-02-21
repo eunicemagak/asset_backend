4e61dd375b5f48a51c69022364a3a113dbc83bee
Ian Ndugire <ian.ndugire@emalify.com>
2022-02-21 08:34:32 +0300

    second commit - user CRUD, Authentication, Middwre...

--- app/http/controllers/assetController.go
+++ app/http/controllers/assetController.go
@@ -2,17 +2,81 @@ package controllers
 
 import (
 	"database/sql"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
+	"gitlab.ci.emalify.com/roamtech/asset_be/app/models"
+	"gitlab.ci.emalify.com/roamtech/asset_be/database"
 )
 
 type AssetController struct {
 	DB *sql.DB
 }
 
+/*
 func (c *AssetController) Index(ctx *fiber.Ctx) error {
 
 	return ctx.JSON(fiber.Map{
 		"message": "Hello world",
 	})
+}
+*/
+
+// func AllAssets(c *fiber.Ctx) error {
+func (c *AssetController) Index(ctx *fiber.Ctx) error {
+	page, _ := strconv.Atoi(ctx.Query("page", "1"))
+
+	return ctx.JSON(models.Paginate(database.DB, &models.Asset{}, page))
+}
+
+func (c *AssetController) CreateAsset(ctx *fiber.Ctx) error {
+	var asset models.Asset
+
+	if err := ctx.BodyParser(&asset); err != nil {
+		return err
+	}
+
+	database.DB.Create(&asset)
+
+	return ctx.JSON(asset)
+}
+
+func (c *AssetController) GetAsset(ctx *fiber.Ctx) error {
+	id, _ := strconv.Atoi(ctx.Params("id"))
+
+	asset := models.Asset{
+		ID: uint(id),
+	}
+
+	database.DB.Find(&asset)
+
+	return ctx.JSON(asset)
+}
+
+func (c *AssetController) UpdateAsset(ctx *fiber.Ctx) error {
+	id, _ := strconv.Atoi(ctx.Params("id"))
+
+	asset := models.Asset{
+		ID: uint(id),
+	}
+
+	if err := ctx.BodyParser(&asset); err != nil {
+		return err
+	}
+
+	database.DB.Model(&asset).Updates(asset)
+
+	return ctx.JSON(asset)
+}
+
+func (c *AssetController) DeleteAsset(ctx *fiber.Ctx) error {
+	id, _ := strconv.Atoi(ctx.Params("id"))
+
+	asset := models.Asset{
+		ID: uint(id),
+	}
+
+	database.DB.Delete(&asset)
+
+	return nil
 }
--- app/models/user.go
+++ app/models/user.go
@@ -1,1 +1,49 @@
 package models
+
+import (
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID        uint           `gorm:"primarykey"`
+	FirstName string         `json:"first_name"`
+	LastName  string         `json:"last_name"`
+	Email     string         `json:"email" gorm:"unique"`
+	Password  []byte         `json:"-"`
+	RoleId    uint           `json:"role_id"`
+	CreatedAt time.Time      `gorm:"index"`
+	UpdatedAt time.Time      `gorm:"index"`
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
+
+func (user *User) SetPassword(password string) {
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(password), 14)
+	user.Password = hashedPassword
+}
+
+// func (user *User) ComparePassword(password string) error {
+// 	return bcrypt.CompareHashAndPassword(user.Password, []byte(password))
+// }
+
+// ComparePassword: Check if the provided password is correct or not
+func (user *User) ComparePassword(password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+}
+
+func (user *User) Count(db *gorm.DB) int64 {
+	var total int64
+	db.Model(&User{}).Count(&total)
+
+	return total
+}
+
+func (user *User) Take(db *gorm.DB, limit int, offset int) interface{} {
+	var assets []User
+
+	db.Preload("Role").Offset(offset).Limit(limit).Find(&assets)
+
+	return assets
+}
